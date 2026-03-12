c28ed5b9897b83c444ba04de6228068e5496f7a5
André Gonzaga <andregonzaga95@gmail.com>
2026-03-12 20:36:50 -0300

    Refactor Footer component structure and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import Linkedin from "../../assets/size-50-linkedin.svg";
 import Figma from "../../assets/size-50-figma.svg";
 
 // style
-import { FooterContainer, NavLink } from "./Footer-Style"
+import { FooterContainer, Contact, NavLink, Social } from "./Footer-Style"
 
 // utils
 import { pxToRem } from "../../utils/pxToRem";
@@ -13,27 +13,41 @@ import { pxToRem } from "../../utils/pxToRem";
 export function Footer(){
     return(
       <FooterContainer>
-        <ul className="flex-center" style={{ gap: pxToRem(64) }}>
-            <li><h2>Meu contato:</h2><NavLink href="tel:+5582987077520">+55 (82) 98707-7520</NavLink></li>
-            <li><h2>Email:</h2><NavLink href="mailto:andregonzaga95@gmail.com">andregonzaga95@gmail.com</NavLink></li>
-        </ul>
-        <ul className="flex-center" style={{ gap: pxToRem(32) }}>
+        <Contact>
+            <li>
+                <h2>Meu contato:</h2>
+                <NavLink href="tel:+5582987077520">
+                    +55 (82) 98707-7520
+                </NavLink>
+            </li>
+
+            <li>
+                <h2>Email:</h2>
+                <NavLink href="mailto:andregonzaga95@gmail.com">
+                    andregonzaga95@gmail.com
+                </NavLink>
+                </li>
+        </Contact>
+
+        <Social>
             <li>
                 <a href="https://github.com/And95" target="_blank" rel="noopener noreferrer">
-                <img src={GitHub} alt="GitHub" />
+                    <img src={GitHub} alt="GitHub" />
                 </a>
             </li>
+
             <li>
                 <a href="https://www.linkedin.com/in/andregonzaga95" target="_blank" rel="noopener noreferrer">
-                <img src={Linkedin} alt="LinkedIn" />
+                    <img src={Linkedin} alt="LinkedIn" />
                 </a>
             </li>
+
             <li>
                 <a href="https://www.figma.com/@And95" target="_blank" rel="noopener noreferrer">
-                <img src={Figma} alt="Figma" />
+                    <img src={Figma} alt="Figma" />
                 </a>
             </li>
-        </ul>
-      </FooterContainer>
+        </Social>
+    </FooterContainer>
     )
 }
