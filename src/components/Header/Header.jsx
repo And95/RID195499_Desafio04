4ce5ac42b37ad524af0acba75d2d317a0c49079d
André Gonzaga <andregonzaga95@gmail.com>
2026-03-12 20:59:06 -0300

    Refactor Header component for improved structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,59 +6,57 @@ import GitHub from "../../assets/size-32-github.svg";
 import Linkedin from "../../assets/size-32-linkedin.svg";
 
 // styles
-import { HeaderContainer, NavLink } from "./Header-Style";
+import { HeaderContainer, DesktopNav, DesktopIcons, NavLink } from "./Header-Style";
 import { HamburgerButton, MobileMenu, Overlay } from "../../styles/mobileButton";
 
-// utils
-import { pxToRem } from "./../../utils/pxToRem";
-
 export function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
 
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-  const closeMenu = () => setMenuOpen(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen)
+  const closeMenu = () => setMenuOpen(false)
 
   return (
     <>
       <HeaderContainer>
-        {/* Desktop Navigation */}
-        <ul className="flex-center desktop-nav" style={{ gap: pxToRem(48) }}>
-          <li><NavLink href="#projects">Projetos</NavLink></li>
-          <li><NavLink href="#technologies">Tecnologias</NavLink></li>
-          <li><NavLink href="#about">Sobre mim</NavLink></li>
-        </ul>
 
-        {/* Social Icons */}
-        <ul className="flex-center desktop-icons" style={{ gap: pxToRem(20) }}>
-          <li>
-            <a href="https://github.com/And95" target="_blank" rel="noopener noreferrer">
-              <img src={GitHub} alt="GitHub" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/andregonzaga95" target="_blank" rel="noopener noreferrer">
-              <img src={Linkedin} alt="LinkedIn" />
-            </a>
-          </li>
-        </ul>
-
-        {/* Hamburger Button */}
+        {/* Hamburger (Mobile) */}
         <HamburgerButton onClick={toggleMenu}>
           <span />
           <span />
           <span />
         </HamburgerButton>
+
+        {/* Desktop Navigation */}
+        <DesktopNav>
+          <li><NavLink href="#projects">Projetos</NavLink></li>
+          <li><NavLink href="#technologies">Tecnologias</NavLink></li>
+          <li><NavLink href="#about">Sobre mim</NavLink></li>
+        </DesktopNav>
+
+        {/* Social Icons */}
+        <DesktopIcons>
+          <li>
+            <a href="https://github.com/And95" target="_blank" rel="noopener noreferrer">
+              <img src={GitHub} alt="GitHub"/>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.linkedin.com/in/andregonzaga95" target="_blank" rel="noopener noreferrer">
+              <img src={Linkedin} alt="LinkedIn"/>
+            </a>
+          </li>
+        </DesktopIcons>
+
       </HeaderContainer>
 
-      {/* Overlay */}
       <Overlay open={menuOpen} onClick={closeMenu} />
 
-      {/* Mobile Menu */}
       <MobileMenu open={menuOpen}>
         <NavLink href="#projects" onClick={closeMenu}>Projetos</NavLink>
         <NavLink href="#technologies" onClick={closeMenu}>Tecnologias</NavLink>
         <NavLink href="#about" onClick={closeMenu}>Sobre mim</NavLink>
       </MobileMenu>
     </>
-  );
+  )
 }
